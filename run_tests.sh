b073904b67bf27e4fb11bb0aaae0b0557c37b8da
Shashank Singh <cse.shashanksingh@gmail.com>
2021-03-28 02:14:33 +0530

    Fixed counter issue in tests

--- run_tests.sh
+++ run_tests.sh
@@ -3,11 +3,12 @@ var1=""
 echo "Please ensure services are running and press a key to start testing!"
 read var1
 
-i=1
+t_count=1
 for FILE in *; 
 do
-	echo "Press any key to start test case $i"
-	bash $FILE; 
+	echo ""
+	echo "------------ Press any key to start Test Case $t_count : $FILE -------"
 	read var1
-	i=i+1
+	bash $FILE; 
+	t_count=$((t_count+1))
 done
